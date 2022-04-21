a87bf523c829d9554e365ea4c40ba24e26419854
wanghazing <wanghong_zz@csii.com.cn>
2022-04-21 21:15:01 +0800

    esbuild

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const TerserPlugin = require("terser-webpack-plugin");
+
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: (config) => {
+    config.optimization = {
+      minimizer: [
+        new TerserPlugin({
+          minify: TerserPlugin.esbuildMinify,
+          terserOptions: {},
+        }),
+      ],
+    };
+  },
+});
